21be16ad6b2e981a744d8dabc7196e56d0ba0c3c
JD Rudie <jd@rudiejd.io>
2024-11-10 23:47:15 -0500

    workin on grep as i learn more stuff

--- elixir/grep/lib/grep.ex
+++ elixir/grep/lib/grep.ex
@@ -4,6 +4,8 @@ defmodule Grep do
   defp search(file, pattern, flags, multi) do 
     {:ok, contents} = File.read(file)
 
+
+
     numbers = Enum.any?(flags, fn f -> f == "-n" end)
     names = Enum.any?(flags, fn f -> f == "-l" end)
     insensitive = Enum.any?(flags, fn f -> f == "-i" end)
@@ -12,11 +14,15 @@ defmodule Grep do
 
     lines = String.split(contents, "\n")
     matches = for n <- 0..length(lines)-1  do
-      cur = 
-        if insensitive, do: String.downcase(Enum.at(lines, n)), else: Enum.at(lines, n)
-      modified_pattern = if insensitive, do: String.downcase(pattern), else: pattern
-      is_match = String.contains?(cur, modified_pattern)
-      if is_match or invert do
+      cur = Enum.at(lines, n)
+      is_match = cond do
+       insensitive  -> String.contains?(String.downcase(cur), String.downcase(pattern))
+       invert and match_entire_line -> cur != pattern
+       match_entire_line -> cur == pattern
+       invert -> not String.contains?(cur, pattern)
+       true -> String.contains?(cur, pattern)
+      end
+      if is_match do 
         if not names do
           "#{if multi, do: "#{file}:", else: ""}#{if numbers, do: "#{n + 1}:", else: "" }#{cur}"
         else
@@ -25,18 +31,19 @@ defmodule Grep do
       end
     end
     matches |>
-    Enum.filter(fn s -> not is_nil(s) and String.length(s) > 0 end) |>
+    Enum.filter(fn s -> not is_nil(s) and String.length(String.trim(s)) > 0 end) |>
     Enum.join("\n")
   end
 
   @spec grep(String.t(), [String.t()], [String.t()]) :: String.t()
   def grep(pattern, flags, files) do
-    res = for file <- files, do: search(file, pattern, flags, length(files) > 1)
-    IO.inspect(res)
-    case length(res) do
+    res = for file <- files, do: search(file, pattern, flags, length(files) > 1) 
+    filtered = Enum.filter(res, fn s -> not is_nil(s) and String.length(String.trim(s)) > 0 end) |> Enum.dedup()
+    IO.inspect(filtered)
+    case length(filtered) do
       0 -> ""
-      1 -> hd(res) <> "\n"
-      _ -> Enum.join(res, "\n")
+      1 -> hd(filtered) <> "\n"
+      _ -> Enum.join(filtered, "\n") |>
     end
   end
 end
